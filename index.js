bb2441dd2e5a5460db82bc8d3c45be65ac820df0
Venkata Sridhar Sai Kalavalapalli <kv.sridharsai@gmail.com>
2022-03-15 19:05:35 +0530

    test

--- index.js
+++ index.js
@@ -2,14 +2,25 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
+
+const fs = require("fs");
+
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
+    console.log(context.payload.issue.body);
+
+    fs.writeFile("sample.txt", context.payload.issue.body, (err) => {
+      if (err) throw err;
+      console.log("Completed!");
+    });
+
     const issueComment = context.issue({
       body: "Thanks for opening this issue!",
     });
+
     return context.octokit.issues.createComment(issueComment);
   });
 
